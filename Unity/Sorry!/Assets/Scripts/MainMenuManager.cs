3cf0c8e3b5bdbe070d542a58431ce22427225654
Connor Pautz <cmp998@mst.edu>
2020-10-18 01:12:35 -0500

    waypoints and initial movement set up / fixed

---
+++ Unity/Sorry!/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static int howManyPlayers;
+
+    public void TwoPlayers() 
+    {
+        howManyPlayers = 2;
+        SceneManager.LoadScene("BoardScene");
+    }
+    public void ThreePlayers() 
+    {
+        howManyPlayers = 3;
+        SceneManager.LoadScene("BoardScene");
+    }
+    public void FourPlayers() 
+    {
+        howManyPlayers = 4;
+        SceneManager.LoadScene("BoardScene");
+    }
+
+    public void Quit()
+    {
+        if(EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+        }
+    }
+}
